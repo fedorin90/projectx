719febb6b9b929c5f50735fdcc9d7fcf41da1bb8
fedorin90 <fedorin.mir@gmail.com>
2023-06-09 14:42:54 +0300

    first work with html template

--- projectx/settings.py
+++ projectx/settings.py
@@ -48,7 +48,8 @@ ROOT_URLCONF = 'projectx.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
